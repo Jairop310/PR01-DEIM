4bc9dcb7d7816cd68dc0c672208503342362d163
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2021-09-17 16:59:52 +0200

    Ejercicio 2 a
    Esto es la creación de un bucle en Update que se para cuando el numero alcanza un determinado numero.

--- 001_Ejercicios/Assets/Scripts/Bucles.cs
+++ 001_Ejercicios/Assets/Scripts/Bucles.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Bucles : MonoBehaviour
 {
+
+    public int num1;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (num1 < 10)
+        {
+            num1++;
+        }
+        else
+        {
+            print("El Numero ha alcanzado 10");
+        }
         
     }
 }
