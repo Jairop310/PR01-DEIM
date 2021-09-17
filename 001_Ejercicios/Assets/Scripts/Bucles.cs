16815035c791339b7fc13ee6c390a3633bad89f4
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2021-09-17 20:36:34 +0200

    Prueba 1 de contador de tiempo
    Esto es una version experimental no es una version fija

--- 001_Ejercicios/Assets/Scripts/Bucles.cs
+++ 001_Ejercicios/Assets/Scripts/Bucles.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class Bucles : MonoBehaviour
 {
 
-    
+    float secundero = 0f;//No esta terminado.
     int aleatorio;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("Reloj");
+
         int fail = 0;
 
         while (aleatorio != 5)
         {
-            aleatorio = Random.Range(0, 500);
+            aleatorio = Random.Range(0, 2000);
             if (aleatorio == 5)
             {
                 print("Ha salido el numero " + aleatorio);
                 print("Ha llegado " + fail + " numero de veces");
+                StopCoroutine("Reloj");
+                print(("ha tardado: ") + secundero);
             }
             else
             {
@@ -32,5 +36,15 @@ public class Bucles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
+
+    IEnumerator Reloj()
+    {
+      
+            while (true)
+        {
+            secundero++;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
