12ab3d9093a8afaea5eebbb04778a16377ce9e4e
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2021-09-20 17:53:33 +0200

    Ejercicio 3A Saludo Personalizado

---
+++ 001_Ejercicios/Assets/Scripts/Ejercicio3.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Saludar("Jairo",19);
+        Saludar("Milena",22);
+        Saludar("Noe",21);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Saludar(string name, int edad )
+    {
+        print("Hola " + name + "tu edad es "+ edad + " años");
+    }
+}
