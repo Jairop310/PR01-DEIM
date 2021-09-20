0716c49be33015edefae44abca64022ec6917d67
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2021-09-20 19:27:58 +0200

    Ejercicio 3B
    En este ejercicio hacemos que una operación determine si el bool este encendido o no dependiendo si el numero es par o impar.

--- 001_Ejercicios/Assets/Scripts/Ejercicio3.cs
+++ 001_Ejercicios/Assets/Scripts/Ejercicio3.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Ejercicio3 : MonoBehaviour
 {
+    public int vida;
     // Start is called before the first frame update
     void Start()
     {
-        Saludar("Jairo",19);
-        Saludar("Milena",22);
-        Saludar("Noe",21);
+        Saludar("Jairo", 19);
+        Saludar("Milena", 22);
+        Saludar("Noe", 21);
+    
     }
 
     // Update is called once per frame
@@ -17,9 +19,9 @@ public class Ejercicio3 : MonoBehaviour
     {
         
     }
-
-    void Saludar(string name, int edad )
+    void Saludar(string name, int edad)
     {
-        print("Hola " + name + "tu edad es "+ edad + " años");
+        print("Hola " + name + "tu edad es " + edad + " años");
     }
 }
+
