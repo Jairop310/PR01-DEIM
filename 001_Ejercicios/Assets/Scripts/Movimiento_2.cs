a43fd7a545de7ee492305818d774ef569ad9e94f
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2021-09-24 17:27:05 +0200

    Movimiento hecho como en el Tutorial y con limitante de espacio

--- 001_Ejercicios/Assets/Scripts/Movimiento_2.cs
+++ 001_Ejercicios/Assets/Scripts/Movimiento_2.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Movimiento_2 : MonoBehaviour
 {
+    [SerializeField] GameObject OtherObject;
     [SerializeField] float speed = 5f;
+    private Vector3 initPos;
+    [SerializeField] Vector3 Destpos;
+    private Vector3 FinalPos;
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +20,16 @@ public class Movimiento_2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float PosX = OtherObject.transform.position.x;
+
         float desp1Y = Input.GetAxis("Vertical") * speed;
         transform.Translate(Vector3.up * desp1Y * Time.deltaTime);
+        if(PosX <= 12)
+        {
+            initPos = OtherObject.transform.position;
+            FinalPos = initPos + Destpos * Time.deltaTime * speed;
+            OtherObject.transform.position = FinalPos;
+        }
         float desp1X = Input.GetAxis("Horizontal") * speed;
         transform.Translate(Vector3.right * desp1X * Time.deltaTime);
     }
